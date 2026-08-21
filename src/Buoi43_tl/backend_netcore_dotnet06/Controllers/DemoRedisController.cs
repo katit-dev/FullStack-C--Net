32fda77d1e510a0533a377ff8cb3a68683e92e4d
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-08-21 14:07:43 +0700

    add GetAllUserRedis api

--- src/Buoi43_tl/backend_netcore_dotnet06/Controllers/DemoRedisController.cs
+++ src/Buoi43_tl/backend_netcore_dotnet06/Controllers/DemoRedisController.cs
@@ -2,6 +2,7 @@ using backend_netcore_dotnet06.Models.DBUser;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
 
 namespace backend_netcore_dotnet06.Controllers
 {
@@ -20,5 +21,44 @@ namespace backend_netcore_dotnet06.Controllers
             _redisService = redisService;
             _userDBContext = userDBContext;
         }
+
+
+
+        [HttpGet("GetAllUserRedis")]
+        public async Task<IActionResult> GetAllUserRedis()
+        {
+            // Kiểm tra key "all_users" có trong Redis không
+            var cachedUsers = await _redisService
+                .GetValueAsync("all_users");
+
+            // Nếu Redis có dữ liệu
+            if (!string.IsNullOrEmpty(cachedUsers))
+            {
+                var users = JsonSerializer
+                    .Deserialize<List<User>>(cachedUsers);
+
+                return Ok(users);
+            }
+
+            // Nếu Redis không có -> lấy từ Database
+            var usersFromDb = await _userDBContext
+                .Users
+                .ToListAsync();
+
+            // Serialize List<User> thành JSON string
+            var jsonUsers = JsonSerializer
+                .Serialize(usersFromDb);
+
+            // Lưu JSON string vào Redis
+            await _redisService
+                .SetValueAsync(
+                    "all_users",
+                    jsonUsers
+                );
+
+            return Ok(usersFromDb);
+        }
+        
+
     }
 }
